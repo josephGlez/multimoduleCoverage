f2aba7aaa889897e4f21253785078f070b7466de
joseph <joseph.gonzalez@upr.edu>
2019-02-11 23:41:26 -0500

    shared test jar setup

---
+++ subProject1/src/test/java/org/joe/exported_tests/exportedTest.java
@@ -0,0 +1,13 @@
+package org.joe.exported_tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class exportedTest {
+
+  @Test
+  public void test() {
+    assertTrue( true );
+    }
+
+}
